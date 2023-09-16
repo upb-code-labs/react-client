97fa77e9cb0193b1edca6747bdb89ae895e13717
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-16 07:08:39 -0500

    chore(dependencies): Replace cypress with playwright

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:5173",
-    setupNodeEvents(_on, _config) {
-    },
-  },
-});
--- cypress/e2e/home.cy.ts
+++
@@ -1,6 +0,0 @@
-describe("Home page tests", () => {
-  it("Should have a button", () => {
-    cy.visit("/")
-    cy.get("button").should("have.text", "Home")
-  })
-})
