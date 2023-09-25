4e9087b4231a707f68fe860c801369d6ff3ecd93
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2023-09-25 09:33:47 -0500

    feat: Register students (#18)
    * feat(ui): Add student's register form UI
    
    * chore(dependencies): Add axios and sonner dependencies
    
    * feat(ui): Add loading state to button component
    
    * feat: Add student's register form logic
    
    * ci: Update docker compose env variables
    
    * test: Add tests for the register form
    
    * ci: Update playwright config
    
    Only use chrome to run the tests
    
    * ci: Install playwright dependencies instead of using docker image

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
+import { Toaster } from "sonner";
+import { RegisterStudent } from "./screens/register-student/RegisterStudent";
 import { Navbar } from "@/components/Navbar/Navbar.tsx";
+import { Home } from "./screens/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global.css";
-import { BrowserRouter } from "react-router-dom";
+import { Login } from "./screens/login/Login";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Toaster expand closeButton richColors />
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register/students" element={<RegisterStudent />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
