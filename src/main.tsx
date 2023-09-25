31410516bca0ad4b4c3a4a7127c023ee8efc01ef
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-25 09:57:19 -0500

    chore: Create empty courses home page

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { Home } from "./screens/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global.css";
 import { Login } from "./screens/login/Login";
+import { CoursesHome } from "./screens/courses-home/CoursesHome";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register/students" element={<RegisterStudent />} />
+        <Route path="/courses" element={<CoursesHome />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
