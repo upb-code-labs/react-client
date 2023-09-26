0a5f3283bc31401c979eb8dfde421f5a6edb0740
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-25 20:06:48 -0500

    feat: Recover session when the page is closes or reloaded

--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { whoamiService } from "@/services/session/session.services";
+import { useEffect, useState } from "react";
 
 export type SessionUser = {
   uuid: string;
@@ -9,8 +10,22 @@ export type SessionUser = {
 export const useSession = () => {
   const [user, setUser] = useState<SessionUser | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-
   const isLoggedIn = !isLoading && !!user;
+
+  useEffect(() => {
+    recoverSession();
+  }, []);
+
+  const recoverSession = async () => {
+    setIsLoading(true);
+
+    const { success, user } = await whoamiService();
+    if (success && user) {
+      setUser(user);
+    }
+
+    setIsLoading(false);
+  };
 
   const login = (user: SessionUser) => {
     setIsLoading(true);
