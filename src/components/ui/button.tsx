92fd8031cee2786177d5a871030fbf2cbb6a344c
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-30 13:57:48 -0500

    style(ui): Use background gradient in primary buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,11 +5,12 @@ import { Loader2 } from "lucide-react";
 import * as React from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors transition-shadow focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:shadow-sm",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-gradient-to-r from-red-upb to-purple-upb text-primary-foreground hover:from-red-upb/90 hover:to-purple-upb/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
