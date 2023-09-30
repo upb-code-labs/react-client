871f6ef3bba04d283ff38d0dade5a9a9d643f877
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-30 18:34:27 -0500

    refactor: Remove unused variables and class names

--- src/components/CourseCard/CourseCard.tsx
+++ src/components/CourseCard/CourseCard.tsx
@@ -7,18 +7,14 @@ interface CourseCardProps {
 
 export const CourseCard = ({ course }: CourseCardProps) => {
   return (
-    <div
-      key={course.uuid}
-      className="mx-auto flex aspect-square w-full max-w-xs flex-col items-center justify-center gap-4 rounded-xl border p-4 text-center shadow-md transition-shadow hover:shadow-lg"
-    >
+    <div className="mx-auto flex aspect-square w-full max-w-xs flex-col items-center justify-center gap-4 rounded-xl border p-4 text-center shadow-md transition-shadow hover:shadow-lg">
       <div
         className="grid aspect-square w-[45%] place-items-center rounded-2xl text-3xl font-bold -tracking-tight text-white"
         style={{ backgroundColor: course.color }}
       >
-        <p className="absolute border-red-500"></p>
         {getCourseInitials(course.name)}
       </div>
-      <span className="line-clamp-2 text-lg">{course.name}</span>
+      <h2 className="line-clamp-2 text-lg">{course.name}</h2>
     </div>
   );
 };
