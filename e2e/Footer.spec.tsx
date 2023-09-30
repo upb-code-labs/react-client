c68ccff0e08187896aa77d023506ca07f2038805
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-30 16:19:43 -0500

    test: Use exact matchers on tests

--- e2e/Footer.spec.tsx
+++ e2e/Footer.spec.tsx
@@ -6,19 +6,24 @@ test("Footer should contain the correct links", async ({ page }) => {
     page.getByLabel("Universidad Pontificia Bolivariana website")
   ).toBeVisible();
   await expect(
-    page.getByRole("link", { name: "Data Processing and Protection Policy" })
-  ).toBeVisible();
-  await expect(page.getByRole("link", { name: "User Manual" })).toBeVisible();
-  await expect(
-    page.getByRole("link", { name: "Organization profile" })
-  ).toBeVisible();
-  await expect(
-    page.getByRole("link", { name: "Frontend repository" })
+    page.getByRole("link", {
+      name: "Data Processing and Protection Policy",
+      exact: true
+    })
   ).toBeVisible();
   await expect(
-    page.getByRole("link", { name: "Main API repository" })
+    page.getByRole("link", { name: "User Manual", exact: true })
   ).toBeVisible();
   await expect(
-    page.getByRole("link", { name: "Tests runner repository" })
+    page.getByRole("link", { name: "Organization profile", exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Frontend repository", exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Main API repository", exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Tests runner repository", exact: true })
   ).toBeVisible();
 });
--- e2e/Navbar.spec.tsx
+++ e2e/Navbar.spec.tsx
@@ -8,8 +8,12 @@ test("App icon should redirect to home page", async ({ page, baseURL }) => {
 
 test("Default navigation options are shown in desktop", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByRole("link", { name: "Login" })).toBeVisible();
-  await expect(page.getByRole("link", { name: "Register" })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Login", exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Register", exact: true })
+  ).toBeVisible();
 });
 
 test("Responsive layout is shown in mobile", async ({ page }) => {
@@ -17,10 +21,18 @@ test("Responsive layout is shown in mobile", async ({ page }) => {
   await page.goto("/");
 
   await page.getByLabel("Open menu").click();
-  await expect(page.getByRole("link", { name: "Login" })).toBeVisible();
-  await expect(page.getByRole("link", { name: "Register" })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Login", exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Register", exact: true })
+  ).toBeVisible();
 
   await page.getByLabel("Close menu").click();
-  await expect(page.getByRole("link", { name: "Login" })).not.toBeVisible();
-  await expect(page.getByRole("link", { name: "Register" })).not.toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Login", exact: true })
+  ).not.toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Register", exact: true })
+  ).not.toBeVisible();
 });
