6ebf00f49bdb38ad1d262b7f51a1ba3021340697
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-30 18:39:57 -0500

    refactor: Wrap courses card in link component

--- src/components/CourseCard/CourseCard.tsx
+++ src/components/CourseCard/CourseCard.tsx
@@ -1,5 +1,6 @@
 import { Course } from "@/types/entities/course";
 import { getCourseInitials } from "@/utils/get-course-initials";
+import { Link } from "react-router-dom";
 
 interface CourseCardProps {
   course: Course;
@@ -7,14 +8,16 @@ interface CourseCardProps {
 
 export const CourseCard = ({ course }: CourseCardProps) => {
   return (
-    <div className="mx-auto flex aspect-square w-full max-w-xs flex-col items-center justify-center gap-4 rounded-xl border p-4 text-center shadow-md transition-shadow hover:shadow-lg">
-      <div
-        className="grid aspect-square w-[45%] place-items-center rounded-2xl text-3xl font-bold -tracking-tight text-white"
-        style={{ backgroundColor: course.color }}
-      >
-        {getCourseInitials(course.name)}
+    <Link to="#">
+      <div className="mx-auto flex aspect-square w-full max-w-xs flex-col items-center justify-center gap-4 rounded-xl border p-4 text-center shadow-md transition-shadow hover:shadow-lg">
+        <div
+          className="grid aspect-square w-[45%] place-items-center rounded-2xl text-3xl font-bold -tracking-tight text-white"
+          style={{ backgroundColor: course.color }}
+        >
+          {getCourseInitials(course.name)}
+        </div>
+        <h2 className="line-clamp-2 text-lg">{course.name}</h2>
       </div>
-      <h2 className="line-clamp-2 text-lg">{course.name}</h2>
-    </div>
+    </Link>
   );
 };
