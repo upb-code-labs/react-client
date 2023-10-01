b372697c7300f9a2e2e2eabff2493cdd1ec960e7
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-01 09:45:53 -0500

    feat: Copy course's invitation code to clipboard

--- src/components/CourseCard/CourseDropDown.tsx
+++ src/components/CourseCard/CourseDropDown.tsx
@@ -8,29 +8,58 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { AuthContext } from "@/context/AuthContext";
 import { SessionRole } from "@/hooks/useSession";
+import { getInvitationCodeService } from "@/services/courses/get-invitation-code.service";
+import { copyToClipboard } from "@/utils/copy-to-clipboard";
 import { ClipboardCopy, EyeOff, MoreVertical, PenSquare } from "lucide-react";
 import { useContext } from "react";
+import { toast } from "sonner";
 
-const getDropdownOptionsByRole = (role: SessionRole) => {
-  if (role == "teacher") {
-    return [
-      {
-        icon: PenSquare,
-        text: "Update name"
-      },
-      {
-        icon: ClipboardCopy,
-        text: "Copy invitation code"
-      }
-    ];
-  } else {
-    return [];
-  }
-};
+interface CourseDropDownProps {
+  courseUUID: string;
+}
 
-export const CourseDropDown = () => {
+export const CourseDropDown = ({ courseUUID }: CourseDropDownProps) => {
   const { user } = useContext(AuthContext);
   const role = user?.role || "student";
+
+  const getDropdownOptionsByRole = (role: SessionRole) => {
+    if (role == "teacher") {
+      return [
+        {
+          icon: PenSquare,
+          text: "Update name",
+          callback: () => console.log("To be implemented")
+        },
+        {
+          icon: ClipboardCopy,
+          text: "Copy invitation code",
+          callback: getInvitationCode
+        }
+      ];
+    } else {
+      return [];
+    }
+  };
+
+  const getInvitationCode = async () => {
+    const { success, ...response } = await getInvitationCodeService(courseUUID);
+    if (!success) {
+      toast.error(response.message);
+      return;
+    }
+
+    const { code } = response;
+    copyInvitationCode(code);
+  };
+
+  const copyInvitationCode = async (code: string) => {
+    const copied = await copyToClipboard(code);
+    if (copied) {
+      toast.success(`Invitation code ${code} copied to clipboard`);
+    } else {
+      toast.error("Couldn't copy invitation code to clipboard");
+    }
+  };
 
   return (
     <DropdownMenu>
@@ -50,7 +79,7 @@ export const CourseDropDown = () => {
           <span>Hide course</span>
         </DropdownMenuItem>
         {getDropdownOptionsByRole(role).map((option) => (
-          <DropdownMenuItem key={option.text}>
+          <DropdownMenuItem key={option.text} onClick={option.callback}>
             <option.icon className="mr-2 h-4 w-4" />
             <span>{option.text}</span>
           </DropdownMenuItem>
