bc6b2df2080ea19c84973b0c439c0d82f85430f4
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-08 09:23:53 -0500

    feat: Centralize courses dialogs state in context

--- src/context/courses/UserCoursesContext.tsx
+++ src/context/courses/UserCoursesContext.tsx
@@ -1,20 +1,39 @@
 import { CoursesActions } from "@/hooks/courses/coursesReducer";
 import { CoursesState, useCourses } from "@/hooks/courses/useCourses";
-import { ReactNode, createContext } from "react";
+import { Course } from "@/types/entities/course";
+import { ReactNode, createContext, useState } from "react";
 
 interface UserCoursesContext {
   isLoading: boolean;
   userCourses: CoursesState;
   userCoursesDispatcher: React.Dispatch<CoursesActions>;
+
+  renameCourseDialogState: RenameCourseDialogState;
+  openRenameCourseDialog: (course: Course) => void;
+  closeRenameCourseDialog: () => void;
 }
 
 const defaultValues: UserCoursesContext = {
+  // Courses state
   isLoading: false,
   userCourses: {
     courses: [],
     hiddenCourses: []
   },
-  userCoursesDispatcher: () => {}
+  userCoursesDispatcher: () => {},
+
+  // Dialogs state
+  openRenameCourseDialog: () => {},
+  closeRenameCourseDialog: () => {},
+  renameCourseDialogState: {
+    isOpen: false,
+    selectedCourse: null
+  }
+};
+
+type RenameCourseDialogState = {
+  isOpen: boolean;
+  selectedCourse: Course | null;
 };
 
 export const UserCoursesContext =
@@ -23,12 +42,35 @@ export const UserCoursesContext =
 export const UserCoursesProvider = ({ children }: { children: ReactNode }) => {
   const { loading, courses, coursesDispatcher } = useCourses();
 
+  // Dialogs state
+  const [renameCourseDialogState, setRenameCourseDialogState] =
+    useState<RenameCourseDialogState>({
+      isOpen: false,
+      selectedCourse: null
+    });
+  const openRenameCourseDialog = (course: Course) => {
+    setRenameCourseDialogState({
+      isOpen: true,
+      selectedCourse: course
+    });
+  };
+
+  const closeRenameCourseDialog = () => {
+    setRenameCourseDialogState({
+      isOpen: false,
+      selectedCourse: null
+    });
+  };
+
   return (
     <UserCoursesContext.Provider
       value={{
         isLoading: loading,
         userCourses: courses,
-        userCoursesDispatcher: coursesDispatcher
+        userCoursesDispatcher: coursesDispatcher,
+        renameCourseDialogState,
+        openRenameCourseDialog,
+        closeRenameCourseDialog
       }}
     >
       {children}
