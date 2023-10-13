bb3c9aff8273964b1174ed2de0dc599775cbf0b2
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-13 10:25:33 -0500

    refactor(ui): Create `GenericTableSkeleton` component
    This component should be used when a table content is loading.

---
+++ src/components/Skeletons/GenericTableSkeleton.tsx
@@ -0,0 +1,48 @@
+import { Skeleton } from "../ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "../ui/table";
+
+interface GenericTableSkeletonProps {
+  rows: number;
+  columns: number;
+  headers: string[];
+}
+
+export const GenericTableSkeleton = ({
+  rows,
+  columns,
+  headers
+}: GenericTableSkeletonProps) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          {headers.map((header) => (
+            <TableHead key={`table-skeleton-${header}`}>{header}</TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {Array(rows)
+          .fill(0)
+          .map((_, i) => (
+            <TableRow key={`table-skeleton-row-${i}`}>
+              {Array(columns)
+                .fill(0)
+                .map((_, j) => (
+                  <TableCell key={`table-skeleton-cell-${i}-${j}`}>
+                    <Skeleton className="h-6 w-full" />
+                  </TableCell>
+                ))}
+            </TableRow>
+          ))}
+      </TableBody>
+    </Table>
+  );
+};
