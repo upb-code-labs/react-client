2c807f31aa0e63f1837bff8b0251263d54408fbc
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-11-14 20:18:21 -0500

    refactor: Update rubric action button component

--- src/components/Rubric/ActionButton.tsx
+++ src/components/Rubric/ActionButton.tsx
@@ -2,14 +2,14 @@ import { Plus } from "lucide-react";
 
 interface ActionButtonProps {
   text: string;
-  onClick: () => void;
+  onClickCallback: () => void;
 }
 
-export const ActionButton = ({ text, onClick }: ActionButtonProps) => {
+export const ActionButton = ({ text, onClickCallback }: ActionButtonProps) => {
   return (
     <button
       className="grid aspect-square w-full max-w-[18rem] flex-shrink-0 place-content-center gap-2 border p-4 text-xl font-medium shadow-md transition-colors hover:shadow-lg sm:w-72"
-      onClick={onClick}
+      onClick={onClickCallback}
     >
       <Plus size={64} strokeWidth={1} className="mx-auto" />
       {text}
