61f45376686ed1250432aaa52224b17db3494604
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-11-18 19:31:29 -0500

    test: Add test to ensure teachers can delete criteria

--- e2e/rubrics/EditRubric.spec.ts
+++ e2e/rubrics/EditRubric.spec.ts
@@ -1,12 +1,11 @@
 import { expect, test } from "@playwright/test";
-
-import rubricData from "./rubric.json" assert { type: "json" };
-
 import {
   getDefaultPassword,
   getDevelopmentAdminCredentials,
   getRandomEmail
 } from "e2e/Utils";
+
+import rubricData from "./rubric.json" assert { type: "json" };
 
 test.describe.serial("Rubrics edition workflow", () => {
   const teacherEmail = getRandomEmail();
@@ -272,5 +271,77 @@ test.describe.serial("Rubrics edition workflow", () => {
         { exact: true }
       )
     ).toHaveValue(newCriteriaWeight);
+  });
+
+  test("Delete criteria", async ({ page }) => {
+    // Login as the teacher
+    await page.goto("/login");
+    await page.getByLabel("Email").fill(teacherEmail);
+    await page.getByLabel("Password").fill(teacherPassword);
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    // Go to the rubrics page
+    await page.getByRole("link", { name: "Rubrics", exact: true }).click();
+    await page.waitForURL(/\/rubrics$/);
+
+    // Click on the edit rubric button
+    await page.getByLabel(`Edit ${rubricName}`).click();
+
+    // Wait for the input with the rubric name to be visible
+    await expect(page.getByLabel("Name")).toBeVisible();
+
+    // Open the confirmation modal
+    const objectiveIndex = 2;
+    const criteriaIndex = 1;
+    await page
+      .getByLabel(
+        `Toggle options for criteria ${criteriaIndex} of objective ${objectiveIndex}`,
+        { exact: true }
+      )
+      .click();
+    await page.getByText("Delete criteria").click();
+
+    // Assert the modal is open
+    const confirmationModalText =
+      "Are you sure you want to delete this criteria?";
+    await expect(
+      page.getByText(confirmationModalText, { exact: true })
+    ).toBeVisible();
+
+    // Confirm the deletion
+    await page.getByText("Proceed").click();
+
+    // Assert the modal is closed
+    await expect(
+      page.getByText(confirmationModalText, { exact: true })
+    ).not.toBeVisible();
+
+    // Assert an alert is shown
+    await expect(
+      page.getByText("The criteria has been deleted successfully")
+    ).toBeVisible();
+
+    // Assert the criteria was deleted
+    const assertCriteriaWasDeleted = () => {
+      expect(
+        page.getByLabel(
+          `Criteria ${criteriaIndex} of objective ${objectiveIndex} description`,
+          { exact: true }
+        )
+      ).not.toBeVisible();
+
+      expect(
+        page.getByLabel(
+          `Criteria ${criteriaIndex} of objective ${objectiveIndex} weight`,
+          { exact: true }
+        )
+      ).not.toBeVisible();
+    };
+
+    assertCriteriaWasDeleted();
+
+    // Reload the page and assert the criteria was deleted
+    await page.reload();
+    assertCriteriaWasDeleted();
   });
 });
