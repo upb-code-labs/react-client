905d92782666253c2ad0b74e9383917cae7b7637
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-11-25 08:33:29 -0500

    fix(ui): Remove courses navigation border on mobile layout

--- src/screens/course-page/CoursePageLayout.tsx
+++ src/screens/course-page/CoursePageLayout.tsx
@@ -41,7 +41,7 @@ export const CoursePageLayout = () => {
 
   return (
     <div className="mx-auto grid min-h-[calc(100vh-5rem)] max-w-7xl auto-rows-min gap-4 p-4 md:auto-rows-auto md:grid-cols-4">
-      <aside className="space-y-8 border-b pb-8 pr-2 md:border-b-0 md:border-r md:pb-0">
+      <aside className="space-y-8 pb-2 pr-2 md:border-r md:pb-0">
         {isLoading ? (
           <CourseNavigationSkeleton />
         ) : (
