7c2d6b7dfa8e5ab3c24afa9d2b274af0b8da92d3
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-11-29 15:12:28 -0500

    fix(ui): Hide laboratory creation dialog according to user role

--- src/screens/course-page/laboratories/CourseLaboratories.tsx
+++ src/screens/course-page/laboratories/CourseLaboratories.tsx
@@ -1,11 +1,15 @@
+import { useSession } from "@/hooks/useSession";
+
 import { CreateLaboratoryDialog } from "./dialogs/create-laboratory/CreateLaboratoryDialog";
 
 export const CourseLaboratories = () => {
+  const { user } = useSession();
+
   return (
     <main className="col-span-3">
       <div className="mb-4 flex flex-col items-start justify-between space-y-4 md:flex-row md:items-center">
         <h1 className="text-3xl font-bold">Course laboratories</h1>
-        <CreateLaboratoryDialog />
+        {user?.role == "teacher" && <CreateLaboratoryDialog />}
       </div>
     </main>
   );
