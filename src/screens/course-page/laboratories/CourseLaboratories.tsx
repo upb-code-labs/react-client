c531784b3a3b80fa0db33235716b32a3256639f0
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-11-30 12:48:20 -0500

    feat: Create table component to show course laboratories

--- src/screens/course-page/laboratories/CourseLaboratories.tsx
+++ src/screens/course-page/laboratories/CourseLaboratories.tsx
@@ -1,9 +1,13 @@
+import { CourseLaboratoriesTable } from "@/components/Laboratories/CourseLaboratoriesTable";
+import { CourseLaboratoriesContext } from "@/context/laboratories/CourseLaboratoriesContext";
 import { useSession } from "@/hooks/useSession";
+import { useContext } from "react";
 
 import { CreateLaboratoryDialog } from "./dialogs/create-laboratory/CreateLaboratoryDialog";
 
 export const CourseLaboratories = () => {
   const { user } = useSession();
+  const { loading, laboratories } = useContext(CourseLaboratoriesContext);
 
   return (
     <main className="col-span-3">
@@ -11,6 +15,7 @@ export const CourseLaboratories = () => {
         <h1 className="text-3xl font-bold">Course laboratories</h1>
         {user?.role == "teacher" && <CreateLaboratoryDialog />}
       </div>
+      <CourseLaboratoriesTable loading={loading} laboratories={laboratories} />
     </main>
   );
 };
