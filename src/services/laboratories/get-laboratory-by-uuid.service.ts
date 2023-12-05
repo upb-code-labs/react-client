979bed0ec3f635fb95139e633330f508bd13f364
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-12-05 15:37:38 -0500

    refactor: Add `type` property to laboratory blocks

--- src/services/laboratories/get-laboratory-by-uuid.service.ts
+++ src/services/laboratories/get-laboratory-by-uuid.service.ts
@@ -1,4 +1,4 @@
-import { Laboratory } from "@/types/entities/laboratory";
+import { Laboratory, MarkdownBlock } from "@/types/entities/laboratory";
 import { AxiosError } from "axios";
 
 import { GenericResponse, HttpRequester } from "../axios";
@@ -22,8 +22,31 @@ export const getLaboratoryByUUIDService = async (
       opening_date: data.opening_date,
       due_date: data.due_date,
       rubricUUID: data.rubric_uuid,
-      blocks: [...data.markdown_blocks, ...data.test_blocks]
+      blocks: []
     };
+
+    // Parse blocks
+    const markdownBlocks: MarkdownBlock[] = data.markdown_blocks.map(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (block: any) => ({
+        uuid: block.uuid,
+        content: block.content,
+        index: block.index,
+        blockType: "markdown"
+      })
+    );
+    laboratory.blocks.push(...markdownBlocks);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const testBlocks: any[] = data.test_blocks.map((block: any) => ({
+      uuid: block.uuid,
+      languageUUID: block.language_uuid,
+      testArchiveUUID: block.test_archive_uuid,
+      name: block.name,
+      index: block.index,
+      blockType: "test"
+    }));
+    laboratory.blocks.push(...testBlocks);
 
     // Sort blocks by index
     laboratory.blocks.sort((a, b) => a.index - b.index);
