f3cab6cb6e2f3616d8236d40a0656c48a59c5884
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2023-12-30 16:08:26 -0500

    feat: Sort enrolled students by full name (#201)
    * feat: Add static files microservice to docker compose file
    
    * feat: Use tanstack table to allow sorting participants table
    
    * style(ui): Update course laboratories table styles to match the participants table
    
    * style(ui): Update rubrics table styles
    
    * style(ui): Fix tables and titles spacing
    
    * fix: Update verb of the endpoint to update MD block content
    
    The endpoint verb was changed from PUT to PATCH

--- src/screens/course-page/laboratories/CourseLaboratories.tsx
+++ src/screens/course-page/laboratories/CourseLaboratories.tsx
@@ -1,8 +1,8 @@
-import { CourseLaboratoriesTable } from "@/components/Laboratories/CourseLaboratoriesTable";
 import { CourseLaboratoriesContext } from "@/context/laboratories/CourseLaboratoriesContext";
 import { useSession } from "@/hooks/useSession";
 import { useContext } from "react";
 
+import { CourseLaboratoriesTable } from "./components/CourseLaboratoriesTable";
 import { CreateLaboratoryDialog } from "./dialogs/create-laboratory/CreateLaboratoryDialog";
 
 export const CourseLaboratories = () => {
@@ -11,11 +11,16 @@ export const CourseLaboratories = () => {
 
   return (
     <main className="col-span-3">
-      <div className="mb-4 flex flex-col items-start justify-between space-y-4 md:flex-row md:items-center">
-        <h1 className="text-3xl font-bold">Course laboratories</h1>
+      <div className="flex flex-col items-start justify-between md:flex-row md:items-center">
+        <h1 className="my-4 text-3xl font-bold">Course laboratories</h1>
         {user?.role == "teacher" && <CreateLaboratoryDialog />}
       </div>
-      <CourseLaboratoriesTable loading={loading} laboratories={laboratories} />
+      <div className="mt-12 md:mt-4">
+        <CourseLaboratoriesTable
+          loading={loading}
+          laboratories={laboratories}
+        />
+      </div>
     </main>
   );
 };
---
+++ src/screens/course-page/participants/components/CourseParticipantsTable.tsx
@@ -0,0 +1,28 @@
+import { GenericTableSkeleton } from "@/components/Skeletons/GenericTableSkeleton";
+import { DataTable } from "@/components/ui/data-table";
+import { EnrolledStudent } from "@/types/entities/enrolled-student";
+import { ColumnDef } from "@tanstack/react-table";
+
+interface CourseParticipantsTableProps {
+  isLoading: boolean;
+  tableColsDefinition: ColumnDef<EnrolledStudent>[];
+  students: EnrolledStudent[];
+}
+
+export const CourseParticipantsTable = ({
+  isLoading,
+  tableColsDefinition,
+  students
+}: CourseParticipantsTableProps) => {
+  if (isLoading) {
+    return (
+      <GenericTableSkeleton
+        columns={3}
+        rows={4}
+        headers={["Full name", "Institutional Id", "Actions"]}
+      />
+    );
+  }
+
+  return <DataTable columns={tableColsDefinition} data={students} />;
+};
