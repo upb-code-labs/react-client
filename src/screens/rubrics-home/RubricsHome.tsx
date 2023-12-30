2aaa04e637ec3f0dbe4488eefec3b7131bd1df8f
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-12-30 15:53:02 -0500

    style(ui): Update rubrics table styles

--- src/screens/rubrics-home/RubricsHome.tsx
+++ src/screens/rubrics-home/RubricsHome.tsx
@@ -1,21 +1,9 @@
-import { GenericTableSkeleton } from "@/components/Skeletons/GenericTableSkeleton";
-import { EmptyContentText } from "@/components/Texts/EmptyContentText";
-import { buttonVariants } from "@/components/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from "@/components/ui/table";
 import { getTeacherRubricsService } from "@/services/rubrics/get-teacher-rubrics.service";
 import { CreatedRubric } from "@/types/entities/rubric";
-import { PenSquare } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { toast } from "sonner";
 
+import { RubricsTable } from "./components/RubricsTable";
 import { CreateRubricDialog } from "./dialogs/CreateRubricDialog";
 
 export const RubricsHome = () => {
@@ -44,57 +32,15 @@ export const RubricsHome = () => {
     setRubrics((prev) => [...prev, rubric]);
   };
 
-  const renderRubrics = () => {
-    if (loading) {
-      return (
-        <GenericTableSkeleton
-          columns={2}
-          rows={4}
-          headers={["Name", "Options"]}
-        />
-      );
-    }
-
-    if (rubrics.length === 0) {
-      return <EmptyContentText text="You don't have any rubrics yet" />;
-    }
-
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Options</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {rubrics.map((rubric) => (
-            <TableRow key={rubric.uuid}>
-              <TableCell className="line-clamp-1">{rubric.name}</TableCell>
-              <TableCell>
-                <Link
-                  to={`/rubrics/${rubric.uuid}`}
-                  aria-label={`Edit ${rubric.name}`}
-                  className={buttonVariants({ variant: "default" })}
-                >
-                  <PenSquare className="mr-2" />
-                  Edit
-                </Link>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
-  };
-
   return (
     <div className="mx-auto max-w-7xl px-4">
       <div className="mb-2 flex flex-col items-start justify-between md:flex-row md:items-center">
         <h1 className="my-4 text-3xl font-bold">Your rubrics</h1>
         <CreateRubricDialog addRubricCallback={addRubric} />
       </div>
-      <main>{renderRubrics()}</main>
+      <main>
+        <RubricsTable isLoading={loading} rubrics={rubrics} />
+      </main>
     </div>
   );
 };
