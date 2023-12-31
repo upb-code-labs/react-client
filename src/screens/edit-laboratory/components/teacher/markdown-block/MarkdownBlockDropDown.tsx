4742ab6f3f019316d567c3511a900df202956ce5
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-12-31 12:07:13 -0500

    refactor: Move laboratory components to a new folder closer to the page

--- src/screens/edit-laboratory/components/teacher/markdown-block/MarkdownBlockDropDown.tsx
+++ src/screens/edit-laboratory/components/teacher/markdown-block/MarkdownBlockDropDown.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger
-} from "../../../components/ui/dropdown-menu";
+} from "../../../../../components/ui/dropdown-menu";
 
 interface MarkdownBlockDropDown {
   blockUUID: string;
