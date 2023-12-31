0ee318d59b5ceeca0126995c034d51d49b4bbd7e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-12-31 15:54:20 -0500

    feat: Download language template from the modal to create new test blocks

--- src/stores/supported-languages-store.tsx
+++ src/stores/supported-languages-store.tsx
@@ -4,11 +4,19 @@ import { create } from "zustand";
 type SupportedLanguagesStore = {
   supportedLanguages: Language[];
   setSupportedLanguages: (supportedLanguages: Language[]) => void;
+  getLanguageNameByUUID: (languageUUID: string) => string;
 };
 
 export const useSupportedLanguagesStore = create<SupportedLanguagesStore>(
-  (set) => ({
+  (set, get) => ({
     supportedLanguages: [],
-    setSupportedLanguages: (supportedLanguages) => set({ supportedLanguages })
+    setSupportedLanguages: (supportedLanguages) => set({ supportedLanguages }),
+    getLanguageNameByUUID: (languageUUID) => {
+      const { supportedLanguages } = get();
+      const language = supportedLanguages.find(
+        (language) => language.uuid === languageUUID
+      );
+      return language?.name ?? "";
+    }
   })
 );
