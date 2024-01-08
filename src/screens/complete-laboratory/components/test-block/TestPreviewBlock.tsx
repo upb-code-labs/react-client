67884f2873a6488ba750823904d2f2ed05debbec
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2024-01-08 11:01:15 -0500

    feat: Submit to test blocks

--- src/screens/complete-laboratory/components/test-block/TestPreviewBlock.tsx
+++ src/screens/complete-laboratory/components/test-block/TestPreviewBlock.tsx
@@ -1,4 +1,6 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TestBlock } from "@/types/entities/laboratory-entities";
+import { useState } from "react";
 
 import { TestPreviewBlockForm } from "./TestPreviewBlockForm";
 
@@ -11,9 +13,35 @@ export const TestPreviewBlock = ({
   block,
   blockIndex
 }: TestPreviewBlockProps) => {
+  const defaultActiveTab = `${block.uuid}-form`;
+  const [activeTab, setActiveTab] = useState(defaultActiveTab);
+
   return (
-    <div className="rounded-sm border p-4">
-      <TestPreviewBlockForm testBlock={block} blockIndex={blockIndex} />
-    </div>
+    <Tabs className="rounded-sm border p-4" value={activeTab}>
+      <TabsList className="mb-2">
+        <TabsTrigger
+          value={`${block.uuid}-form`}
+          onClick={() => setActiveTab(`${block.uuid}-form`)}
+          aria-label={`Test block ${blockIndex + 1} submission form`}
+        >
+          Submission Form
+        </TabsTrigger>
+        <TabsTrigger
+          value={`${block.uuid}-status`}
+          onClick={() => setActiveTab(`${block.uuid}-status`)}
+          aria-label={`Test block ${blockIndex + 1} submission status`}
+        >
+          Submission Status
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value={`${block.uuid}-form`}>
+        <TestPreviewBlockForm
+          testBlock={block}
+          blockIndex={blockIndex}
+          changeToStatusTabCallback={() => setActiveTab(`${block.uuid}-status`)}
+        />
+      </TabsContent>
+      <TabsContent value={`${block.uuid}-status`}>Pending...</TabsContent>
+    </Tabs>
   );
 };
---
+++ src/services/submissions/submit-to-test-block.service.ts
@@ -0,0 +1,49 @@
+import { AxiosError } from "axios";
+
+import { GenericResponse, HttpRequester } from "../axios";
+
+type submitToTestBlockRequest = {
+  testBlockUUID: string;
+  submissionArchive: File;
+};
+
+type submitToTestBlockResponse = GenericResponse & {
+  uuid: string;
+};
+
+export async function submitToTestBlockService({
+  testBlockUUID,
+  submissionArchive
+}: submitToTestBlockRequest): Promise<submitToTestBlockResponse> {
+  const { axios } = HttpRequester.getInstance();
+
+  try {
+    // Create the multipart form data
+    const formData = new FormData();
+    formData.append("submission_archive", submissionArchive);
+
+    // Send the request
+    const response = await axios.post(
+      `/submissions/${testBlockUUID}`,
+      formData
+    );
+    return {
+      success: true,
+      message: "Your code has been submitted successfully",
+      uuid: response.data.uuid
+    };
+  } catch (error) {
+    let errorMessage = "There was an error submitting your code";
+
+    if (error instanceof AxiosError) {
+      const { message } = error.response?.data || "";
+      if (message) errorMessage = message;
+    }
+
+    return {
+      success: false,
+      message: errorMessage,
+      uuid: ""
+    };
+  }
+}
