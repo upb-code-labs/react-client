73e5981d9ab5e56d02dc1c64e8eb11dfc93470b5
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2024-01-12 15:39:19 -0500

    refactor: Update query to create new rubrics

--- src/screens/rubrics-list/RubricsHome.tsx
+++ src/screens/rubrics-list/RubricsHome.tsx
@@ -1,14 +1,11 @@
 import { getTeacherRubricsService } from "@/services/rubrics/get-teacher-rubrics.service";
-import { CreatedRubric } from "@/types/entities/rubric-entities";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 import { RubricsTable } from "./components/RubricsTable";
 import { CreateRubricDialog } from "./dialogs/CreateRubricDialog";
 
 export const RubricsHome = () => {
-  const queryClient = useQueryClient();
-
   const {
     data: rubrics,
     isLoading: isRubricsLoading,
@@ -19,12 +16,6 @@ export const RubricsHome = () => {
     queryFn: getTeacherRubricsService
   });
 
-  const addRubric = (rubric: CreatedRubric) => {
-    queryClient.setQueryData(["rubrics"], (oldData: CreatedRubric[]) => {
-      return [...oldData, rubric];
-    });
-  };
-
   // Handle error state
   if (isRubricsError) {
     toast.error(rubricsError.message);
@@ -34,7 +25,7 @@ export const RubricsHome = () => {
     <div className="mx-auto max-w-7xl px-4">
       <div className="mt-4 flex flex-col items-start justify-between md:flex-row md:items-center">
         <h1 className="my-4 text-3xl font-bold">Your rubrics</h1>
-        <CreateRubricDialog addRubricCallback={addRubric} />
+        <CreateRubricDialog />
       </div>
       <main className="mt-8 md:mt-4">
         <RubricsTable isLoading={isRubricsLoading} rubrics={rubrics} />
