53a0984a8182ce747f956f270c88a7a5e7f7ff00
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2024-01-13 10:47:39 -0500

    feat: Delete rubric (#240)
    * feat: Delete rubric
    
    * test: Add test to ensure teachers can delete rubrics

--- src/screens/rubrics-list/components/RubricsTable.tsx
+++ src/screens/rubrics-list/components/RubricsTable.tsx
@@ -1,5 +1,5 @@
 import { GenericTableSkeleton } from "@/components/Skeletons/GenericTableSkeleton";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -9,15 +9,41 @@ import {
   TableRow
 } from "@/components/ui/table";
 import { CreatedRubric } from "@/types/entities/rubric-entities";
-import { PenSquareIcon } from "lucide-react";
+import { PenSquareIcon, TrashIcon } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
+import { DeleteRubricDialog } from "../dialogs/DeleteRubricDialog";
 
 interface rubricsTableProps {
   isLoading: boolean;
   rubrics: CreatedRubric[] | undefined;
 }
 
+type deleteRubricDialogState = {
+  isDeleteRubricModalOpen: boolean;
+  selectedRubricUUID: string | undefined;
+};
+
 export const RubricsTable = ({ isLoading, rubrics }: rubricsTableProps) => {
+  // Deletion dialog state
+  const [deleteRubricDialogState, setDeleteRubricDialogState] =
+    useState<deleteRubricDialogState>({
+      isDeleteRubricModalOpen: false,
+      selectedRubricUUID: undefined
+    });
+
+  const { selectedRubricUUID, isDeleteRubricModalOpen } =
+    deleteRubricDialogState;
+
+  const setIsDeleteRubricModalOpen = (isOpen: boolean) => {
+    setDeleteRubricDialogState((prevState) => ({
+      ...prevState,
+      isDeleteRubricModalOpen: isOpen
+    }));
+  };
+
+  // Handle loading state
   if (isLoading) {
     return (
       <GenericTableSkeleton
@@ -29,40 +55,63 @@ export const RubricsTable = ({ isLoading, rubrics }: rubricsTableProps) => {
   }
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Options</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {rubrics?.length ? (
-            rubrics.map((rubric) => (
-              <TableRow key={rubric.uuid}>
-                <TableCell className="line-clamp-1">{rubric.name}</TableCell>
-                <TableCell>
-                  <Link
-                    to={`/rubrics/${rubric.uuid}`}
-                    aria-label={`Edit ${rubric.name}`}
-                    className={buttonVariants({ variant: "default" })}
-                  >
-                    <PenSquareIcon className="mr-2" />
-                    Edit
-                  </Link>
+    <>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Options</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rubrics?.length ? (
+              rubrics.map((rubric) => (
+                <TableRow key={rubric.uuid}>
+                  <TableCell className="line-clamp-1">{rubric.name}</TableCell>
+                  <TableCell className="w-min">
+                    <div className="flex max-w-sm flex-wrap gap-4">
+                      <Link
+                        to={`/rubrics/${rubric.uuid}`}
+                        aria-label={`Edit ${rubric.name}`}
+                        className={buttonVariants({ variant: "default" })}
+                      >
+                        <PenSquareIcon className="mr-2" />
+                        Edit
+                      </Link>
+                      <Button
+                        variant="destructive"
+                        aria-label={`Delete ${rubric.name}`}
+                        onClick={() => {
+                          setDeleteRubricDialogState((prevState) => ({
+                            ...prevState,
+                            selectedRubricUUID: rubric.uuid,
+                            isDeleteRubricModalOpen: true
+                          }));
+                        }}
+                      >
+                        <TrashIcon className="mr-2" />
+                        Delete
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={2} className="h-24 text-center">
+                  No results.
                 </TableCell>
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={2} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DeleteRubricDialog
+        rubricUUID={selectedRubricUUID}
+        isDialogOpen={isDeleteRubricModalOpen}
+        setIsDialogOpen={setIsDeleteRubricModalOpen}
+      />
+    </>
   );
 };
