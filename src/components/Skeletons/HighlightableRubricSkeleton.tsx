bea9e80661905b7d3cec16d7b9ecd1ae11359f99
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2024-01-20 21:45:37 -0500

    refactor: Rename grading rubric skeleton

--- src/components/Skeletons/HighlightableRubricSkeleton.tsx
+++ src/components/Skeletons/HighlightableRubricSkeleton.tsx
@@ -1,6 +1,6 @@
 import { RubricObjectivesSkeleton } from "@/screens/edit-rubric/skeletons/RubricObjectivesSkeleton";
 
-export const GradingRubricSkeleton = () => {
+export const HighlightableRubricSkeleton = () => {
   return (
     <div className="flex max-w-[calc(100vw-2rem)] flex-col gap-4 md:col-span-3">
       {/* rubric skeleton */}
