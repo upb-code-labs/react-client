b1726a1c9f0bf27b0234c92facb984deccad04cd
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2024-02-08 12:55:23 -0500

    release: v0.50.2 (#286)
    * fix: Show confirmation dialog when deleting laboratory blocks (#285)
    
    * chore(ui): Hide open source section in the home page
    
    * fix: Show confirmation dialog when deleting laboratory blocks

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,13 +1,14 @@
 import { HomeFeatures } from "./components/HomeFeatures";
 import { HomeHeader } from "./components/HomeHeader";
-import { OpenSourceSection } from "./components/OpenSourceSection";
+
+/* import { OpenSourceSection } from "./components/OpenSourceSection"; */
 
 export const Home = () => {
   return (
     <main>
       <HomeHeader />
       <HomeFeatures />
-      <OpenSourceSection />
+      {/* <OpenSourceSection /> */}
     </main>
   );
 };
