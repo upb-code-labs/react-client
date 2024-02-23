1883d6e7cfc077416ebcb2cf9bea4884f10cfb5d
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2024-02-23 17:10:16 -0500

    test: Mark terms and conditions checkbox on tests

--- e2e/courses/EnrollStudent.spec.ts
+++ e2e/courses/EnrollStudent.spec.ts
@@ -57,6 +57,7 @@ test.describe.serial("Enroll student workflow", () => {
     await page.getByLabel("Institutional ID").fill(studentInstitutionalID);
     await page.getByLabel("Email").fill(studentEmail);
     await page.getByLabel("Password").fill(studentPassword);
+    await page.getByLabel("Accept terms and conditions").check();
     await page.getByRole("button", { name: "Submit" }).click();
   });
 
